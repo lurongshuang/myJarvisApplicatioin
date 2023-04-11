24c0193a1d97c7763b8582eaf1c3b6f283ae8fbb
卢融霜 <13269779672@163.com>
2023-04-11 20:05:44 +0800

    调整

--- app/src/main/java/snow/music/util/SoundPoolUtils.java
+++ app/src/main/java/snow/music/util/SoundPoolUtils.java
@@ -1,8 +1,10 @@
 package snow.music.util;// Created byjinengmao
 
 import android.content.Context;
+import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
+import android.os.Build;
 
 
 import java.util.HashMap;
@@ -18,7 +20,17 @@ public class SoundPoolUtils {
     private HashMap<Integer, Integer> soundMap = new HashMap<Integer, Integer>();
 
     public void init(Context context) {
-        soundPool = new SoundPool(100, AudioManager.STREAM_SYSTEM, 5);
+        if (Build.VERSION.SDK_INT >= 21) {
+            SoundPool.Builder builder = new SoundPool.Builder();
+            builder.setMaxStreams(100);
+            AudioAttributes.Builder attrBuilder = new AudioAttributes.Builder();
+            attrBuilder.setLegacyStreamType(AudioManager.STREAM_MUSIC);
+            builder.setAudioAttributes(attrBuilder.build());
+            soundPool = builder.build();
+        } else {
+            soundPool = new SoundPool(100, AudioManager.STREAM_SYSTEM, 5);
+        }
+
         if (soundMap.size() > 0) {
             soundMap.clear();
         }
